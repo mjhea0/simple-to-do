8f38c385e23aaa3f42dc7fb7fa5896a0871daacd
RU-013 <RU-013@Admins-Mac-mini-2.local>
2013-09-20 08:50:54 -0600

    initial

---
+++ list.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	var listObject = new Object();
+
+	$('input#item').focus();
+	function addRow(user_number,user_item){
+		var newNumber = "<td>"+user_number+"</td>";
+		var newItem= "<td>" +user_item+"</td>";
+		var newCheck= "<td> <div class='box'></div</td>";
+		$("table#my_list tbody").append("<tr>"+newItem +newNumber+newCheck+"</tr>");
+	};
+
+	
+
+	if(localStorage.getItem('list')){
+		var getItems= JSON.parse(localStorage.getItem('list'))
+		for (var i in getItems){
+			addRow(getItems[i], i)
+		}
+		$('.box').on('click', function(){
+			$(this).parents('tr').remove()
+		})
+	}
+	
+	$('input').keypress(function(e){
+		if(e.which== 13){
+			event.preventDefault();
+			var user_number=$("#number").val();
+			var user_item=$("#item").val();
+
+			listObject[user_item] = user_number
+			addRow(user_number,user_item);	
+			document.forms[0].reset();
+			$('input#item').focus();
+			
+			localStorage.setItem('list', JSON.stringify(listObject))
+			
+			$('.box').on('click', function(){
+				$(this).parents('tr').remove()
+			})
+		}
+	});
+});
